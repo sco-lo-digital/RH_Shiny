6ef3290bfb9beb7dbb5128ff3d4b959564d43931
sco-lo-digital <scottjacobs@gmail.com>
2016-06-04 13:54:45 -0700

    init

--- server.R
+++ server.R
@@ -1,16 +1,75 @@
 library(shiny)
+library(knitr)
+library(ggplot2)
+library(ggthemes)
+library(formattable)
+library(shinydashboard)
+
 
 load("rh.roc.RData")
-# Define server logic required to draw a histogram
+
+
 shinyServer(function(input, output) {
-  
-  output$text1 <- renderText({ 
-        paste("The number of TPs at this rate is", tps <- rh_roc[rh_roc$True.Positive.Rates == input$TP,"True.Positive.Scores"])
+    
+   
+  output$table1 <- renderFormattable({ 
+        tps <- rh_roc[rh_roc$Accuracy == input$acc,"True.Positive.Scores"] 
+        tns <- rh_roc[rh_roc$Accuracy == input$acc,"True.Negative.Scores"]
+        fps <- rh_roc[rh_roc$Accuracy == input$acc,"False.Predictive.Scores"]
+        fns <- rh_roc[rh_roc$Accuracy == input$acc,"False.Negative.Scores"]
+        
+        conf_matrix <- data.frame(TrueNegative = as.numeric(tns), FalsePositive =fps, FalseNegative = fns, TruePositive = tps, stringsAsFactors = F)
+#         conf_matrix <- data.frame(rbind(c(tns, fps),c(fns, tps)))
+#         names(conf_matrix) <- c("Predicted False", "Predicted True")
+#         row.names(conf_matrix) <- c("Actually False", "Actually True")
+        
+       formattable(conf_matrix, list(
+           TrueNegative = color_tile("white", "orange"),
+           FalsePositive = color_tile("white", "orange"),
+           FalseNegative = color_tile("white", "orange"),
+           TruePositive = color_tile("white", "orange")
+        )
+        )
+       
   
                               }
                             )
+  
   output$text2 <- renderText({ 
-        paste("The expected revenue at the avg. value you've entere is", revs <- tps * input$revenue)
+      tps <- rh_roc[rh_roc$Accuracy == input$acc,"True.Positive.Scores"]
+      tns <- rh_roc[rh_roc$Accuracy == input$acc,"True.Negative.Scores"]
+      fps <- rh_roc[rh_roc$Accuracy == input$acc,"False.Predictive.Scores"]
+      fns <- rh_roc[rh_roc$Accuracy == input$acc,"False.Negative.Scores"]
+      tot_rev <- tps * input$revenue
+      tot_cost <-  (tps + fps) * input$cost 
+      paste("The ratio of Revenue to Cost is ", prettyNum(tot_rev / tot_cost, big.mark = ",", digits = 2))
+      
+        }
+  )
+  
+  
+  output$text3 <- renderText({ 
+      tps <- rh_roc[rh_roc$Accuracy == input$acc,"True.Positive.Scores"]
+      tns <- rh_roc[rh_roc$Accuracy == input$acc,"True.Negative.Scores"]
+      fps <- rh_roc[rh_roc$Accuracy == input$acc,"False.Predictive.Scores"]
+      fns <- rh_roc[rh_roc$Accuracy == input$acc,"False.Negative.Scores"]
+      tot_rev <- tps * input$revenue
+      tot_cost <-  (tps + fps) * input$cost 
+      paste("The Net Profit is ", prettyNum(tot_rev - tot_cost, big.mark = ","))
+      
+  }
+  )
+  
+  output$plot1 <- renderPlot({ 
+        tps <- rh_roc[rh_roc$Accuracy == input$acc,"True.Positive.Scores"]
+        tns <- rh_roc[rh_roc$Accuracy == input$acc,"True.Negative.Scores"]
+        fps <- rh_roc[rh_roc$Accuracy == input$acc,"False.Predictive.Scores"]
+        fns <- rh_roc[rh_roc$Accuracy == input$acc,"False.Negative.Scores"]
+        tot_rev <- tps * input$revenue
+        tot_cost <-  (tps + fps) * input$cost 
+        df <- data.frame(Financials = factor(c("Revenue", "Cost"),levels = c("Revenue", "Cost")), Values = c(tot_rev, tot_cost))
+        ggplot(df, aes(x= Financials, y = Values, fill = Financials)) + geom_bar(stat = "identity") + theme(legend.position="none") + theme_fivethirtyeight() + scale_fill_fivethirtyeight()
+        #barplot(c(tot_rev, tot_cost), names.arg = c("Revenue", "Cost"))
     
   }
   )
--- ui.R
+++ ui.R
@@ -1,24 +1,36 @@
 library(shiny)
+load("rh.roc.RData")
 # Define UI for
 shinyUI(fluidPage(
   # Application title
   titlePanel(img(src="restorationhardware.png", height = 100, width = 100)),
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
-    sidebarPanel( h3("Calibrate Model"),
-                  selectInput("TP",
-                              label = h5("True Positive Rate"), 
-                              choices = rh_roc$True.Positive.Rates),
-                  h3("Calibrate Business Considerations"),
-                  numericInput("cost",
-                               label = h5("Cost of Sending Catalog\n ($/catalog)"), 
-                               value = 40.00),
-                  numericInput("revenue",
-                               label = h5("Average value of Teen order ($)"), 
-                               value = 300.00)
+    sidebarPanel(   h3("Calibrate Business Considerations"),
+                    #cost
+                    numericInput("cost",
+                                 label = h5("Cost of Sending Catalog\n ($/catalog)"), 
+                                 value = 10.00),
+                    #revenue
+                    numericInput("revenue",
+                                 label = h5("Average value of Teen order ($/order)"), 
+                                 value = 300.00),
+                    h3("Calibrate Model"),
+                    h5("Deploying the model requires selecting a specified level of accuracy, which correlates to a likelihood threshold. At a higher level of accuracy, there will be a larger proportion of TP to FP, however, 
+                     the absolute number of TPs is sacrificed. Try relaxing the Accuracy and observe the effects
+                     on TPs and FNs"),
+                    selectInput("acc",
+                              label = h5("Accuracy"), 
+                              choices = rh_roc$Accuracy)
+                 
+                  
                   ),
-    mainPanel(h1("Summary"),
-              textOutput("text1"),
-              textOutput("text2"))
+    mainPanel(h1("Confusion Matrix for eXtreme Gradient Boosted Trees with Early Stopping Model"),
+              formattableOutput("table1"),
+              textOutput("text2"),
+              textOutput("text3"),
+              plotOutput("plot1")
+              
+              )
   )
 ))
